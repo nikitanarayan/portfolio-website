e43df7dcbfa2185691e1f9a40e24770d9dc836cd
Quentin Creuzet <quentin@f451.faith>
2021-11-02 21:28:43 +0100

    Data

--- www/site/templates/datavisualisation.php
+++ www/site/templates/datavisualisation.php
@@ -1,3 +1,22 @@
 <?php snippet('head') ?>
 
+<section class="data content">
+  <article class="data__inner">
+    <?php foreach ($page->blocks()->toBlocks() as $block) : ?>
+      <div class="data__block">
+        <div class="data__col data__col--image">
+          <div class="data__image">
+            <img src="<?= $block->image()->toFile()->url() ?>">
+          </div>
+        </div>
+        <div class="data__col data__col--text">
+          <div class="data__quote"><?= $block->quote()->kt() ?></div>
+          <div class="data__text"><?= $block->text()->kt() ?></div>
+          <div class="data__source">Source: <?= $block->quote()->kirbytextinline() ?></div>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </article>
+</section>
+
 <?php snippet('foot') ?>
